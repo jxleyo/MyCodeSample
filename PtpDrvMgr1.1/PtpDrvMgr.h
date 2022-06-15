ce815f4ef328440a03ff62559b283e362df2366a
jxleyo.HRP <dyhrp@hotmail.com>
2022-06-15 16:36:00 +0800

    Add files via upload

---
+++ PtpDrvMgr1.1/PtpDrvMgr.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "resource.h"
+
+
+
+//
+#include <strsafe.h>
+
+#include <setupapi.h>
+#include <cfgmgr32.h>
+#include <newdev.h>
+
+#include <hidsdi.h>
+
+#include <atlstr.h>
+#include <shellapi.h>
+
+
+
+
+#define PtpDrvMgr_FLAG_FORCE       0x00000001
+
+// exit codes
+#define EXIT_OK      (0)
+#define EXIT_REBOOT  (1)
+#define EXIT_FAIL    (2)
+#define EXIT_USAGE   (3)
+
+
+BOOL EnbalePrivileges();
+BOOL EnumerateDevices();
+BOOL Rescan();
+BOOL FindDevice();
+BOOL RemoveDriver();
+BOOL UpdateDriver();
+BOOL UninstallDriver();
+BOOL InstallDriver();
+void Install();
+void Uninstall();
+
+BOOL DirExist(LPCWSTR szFilePathName);
+BOOL FileExist(LPCWSTR szFilePathName);
+BOOL MainFileExist(LPCWSTR szFileName);
+BOOL DrvFileExist(LPCWSTR szFileName);
+BOOL LogFileExist(LPCWSTR szFileName);
+BOOL NewLogFile(LPCWSTR szFileName);
+void DelLogFile(LPCWSTR szFileName);
+BOOL GetTouchPad_I2C_hwID();
+BOOL GetOEMDriverName();
+BOOL SaveOEMDriverName();
+BOOL SaveTouchPad_I2C_hwID();
+
+
+wchar_t* mystrcat(const wchar_t* str1, const wchar_t* str2, const wchar_t* str3);//str1是原来的字符串，str2是str1中的某段需要替换字符串，str3是替换str2的字符串
+BOOL FuzzyCompareHwIds(PZPWSTR Array, const wchar_t* MatchHwId);
+BOOL FindCurrentDriver(_In_ HDEVINFO Devs, _In_ PSP_DEVINFO_DATA DevInfo, _In_ PSP_DRVINFO_DATA DriverInfoData);
+BOOL GetDeviceOEMdriverInfo(_In_ HDEVINFO Devs, _In_ PSP_DEVINFO_DATA DevInfo);
+void Set_InstallationSources_Directory(wchar_t* szPath);
+
+void DelMultiSz(_In_opt_ __drv_freesMem(object) PZPWSTR Array);
+__drv_allocatesMem(object) LPTSTR* GetDevMultiSz(_In_ HDEVINFO Devs, _In_ PSP_DEVINFO_DATA DevInfo, _In_ DWORD Prop);
+__drv_allocatesMem(object) LPTSTR* GetMultiSzIndexArray(_In_ __drv_aliasesMem LPTSTR MultiSz);
+
+
+void CreateDlgWnd();
+void ShowDlgWnd();
+void SetWndTop();
+BOOL AddTrayIcon(HINSTANCE hInstance);
+void ShowTrayMsg();
+
---
+++ PtpDrvMgr1.1/resource.h
@@ -0,0 +1,65 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 PtpDrvMgr.rc 使用
+//
+#define IDC_MYICON                      2
+#define IDD_PTPDRVMGR_DIALOG            102
+#define IDS_APP_TITLE                   103
+#define IDM_EXIT                        105
+#define IDI_PTPDRVMGR                   107
+#define IDI_SMALL                       108
+#define IDC_PTPDRVMGR                   109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG_PTPDRVMGR            129
+#define IDC_TRAYMENU                    130
+#define IDD_DIALOG_SETTING              132
+#define IDD_DIALOG_REG                  133
+#define IDD_DIALOG_ABOUT                134
+#define IDC_STATIC_VER                  1001
+#define IDC_STATIC_COPYRIGHT            1002
+#define IDC_STATIC_ICONSMALL            1003
+#define IDC_STATIC_ABOUTINFO            1004
+#define IDC_STATIC_SET_SENS             1005
+#define IDC_STATIC_SET_WHEELENABLE      1006
+#define IDC_STATIC_WHEELMODE            1007
+#define IDC_STATIC_OPMODE               1008
+#define IDC_STATIC_STATUS               1009
+#define IDC_MANUAL                      1010
+#define IDC_VIDEOTUTOR                  1011
+#define IDC_FUNCDEF                     1012
+#define IDC_OFFICIAL                    1013
+#define IDC_STATIC_DEFSMALL             1014
+#define IDC_STATIC_DEFPIC               1015
+#define IDC_STATIC_PAY                  1016
+#define IDC_STATIC_PAYALHEAD            1017
+#define IDC_STATIC_PAYALINFO            1018
+#define IDC_STATIC_REGSN                1019
+#define IDC_STATIC_REGS                 1020
+#define IDC_EDIT_REGINPUT               1021
+#define IDC_BUTTON_REGOK                1022
+#define IDC_STATIC_REGINFO              1023
+#define IDC_STATIC_REGUSER              1024
+#define IDC_SYSLINK_EULA                1025
+#define ID_OPMETHOD                     32779
+#define ID_WHEELMODE                    32780
+#define ID_SENS                         32781
+#define ID_FUNCDEF                      32782
+#define ID_MANULE                       32783
+#define ID_MANUAL                       32784
+#define ID_VIDEO                        32785
+#define ID_VIDEOTUR                     32786
+#define ID_ABOUT                        32787
+#define ID_WHEELENABLE                  32788
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_COMMAND_VALUE         32790
+#define _APS_NEXT_CONTROL_VALUE         1026
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
