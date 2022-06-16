12c9772d19d00b88c085b73effbf32d436bdad46
jxleyo.HRP <dyhrp@hotmail.com>
2022-06-16 16:08:26 +0800

    Add files via upload

---
+++ MouseLikeTouchPad_TraySvc/resource.h
@@ -0,0 +1,63 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MouseLikeTouchPadTraySvc.rc 使用
+//
+#define IDC_MYICON                      2
+#define IDD_MOUSELIKETOUCHPAD_TRAYSVC_DIALOG 102
+#define IDS_APP_TITLE                   103
+#define IDI_SMALL                       108
+#define IDR_MAINFRAME                   128
+#define IDR_TRAYMENU                    130
+#define IDD_DIALOG_ABOUT                131
+#define IDD_DIALOG_SETTING              132
+#define IDD_DIALOG_REG                  133
+#define IDC_STATIC_VER                  1001
+#define IDC_STATIC_COPYRIGHT            1002
+#define IDC_STATIC_ICONSMALL            1003
+#define IDC_STATIC_ABOUTINFO            1004
+#define IDC_STATIC_SET_SENS             1005
+#define IDC_STATIC_SET_WHEELENABLE      1006
+#define IDC_STATIC_WHEELMODE            1007
+#define IDC_STATIC_OPMODE               1008
+#define IDC_STATIC_STATUS               1009
+#define IDC_MANUAL                      1010
+#define IDC_VIDEOTUTOR                  1011
+#define IDC_FUNCDEF                     1012
+#define IDC_OFFICIAL                    1013
+#define IDC_STATIC_DEFSMALL             1014
+#define IDC_STATIC_DEFPIC               1015
+#define IDC_STATIC_PAY                  1016
+#define IDC_STATIC_PAYALHEAD            1017
+#define IDC_STATIC_PAYALINFO            1018
+#define IDC_STATIC_REGSN                1019
+#define IDC_STATIC_REGS                 1020
+#define IDC_EDIT_REGINPUT               1021
+#define IDC_BUTTON_REGOK                1022
+#define IDC_STATIC_REGINFO              1023
+#define IDC_STATIC_REGUSER              1024
+#define IDC_SYSLINK_EULA                1025
+#define IDM_EXIT                        32776
+#define ID_OPMETHOD                     32779
+#define ID_WHEELMODE                    32780
+#define ID_SENS                         32781
+#define ID_FUNCDEF                      32782
+#define ID_MANULE                       32783
+#define ID_MANUAL                       32784
+#define ID_VIDEO                        32785
+#define ID_VIDEOTUR                     32786
+#define ID_ABOUT                        32787
+#define ID_WHEELENABLE                  32788
+
+#define WM_SYSTEMTRAY         (WM_USER+103) //响应鼠标在托盘图标上的事件
+
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
