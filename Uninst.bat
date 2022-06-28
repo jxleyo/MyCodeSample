5b9a6ac8a5c34f5ba39adec0b7d5c68a73f0cad9
jxleyo.HRP <dyhrp@hotmail.com>
2022-06-28 21:27:55 +0800

    Add files via upload

--- Uninst.bat
+++ Uninst.bat
@@ -42,7 +42,7 @@ if not exist LogFIle (
 
 if not exist LogFIle\OEMDriverName.txt (
      echo 获取OEM驱动文件名失败。
-     echo Failed to unload the driver. 
+     echo Failed to get OEMDriverName. 
      echo OEM_FAILED >LogFIle\Return_UninstDrv.txt
      echo.
      exit
@@ -69,6 +69,39 @@ echo.
 pnputil /enum-devices /class {745a17a0-74d3-11d0-b6fe-00a0c90f57da}  /ids /relations >LogFIle\hid_dev.txt
  echo.
  
+find/i "MouseLikeTouchPad_I2C" LogFIle\hid_dev.txt || (
+     goto unInstSucceded
+)
+
+
+if not exist LogFIle\TouchPad_I2C_devInstanceID.txt (
+     echo 获取设备实例失败。
+     echo Failed to get devInstanceID. 
+     echo devInstanceID_FAILED >LogFIle\Return_UninstDrv.txt
+     echo.
+     exit
+)
+
+  ::从TouchPad_I2C_devInstanceID.txt文件读取设备实例
+  for /f "delims=" %%i in (LogFIle\TouchPad_I2C_devInstanceID.txt) do (
+   set "i2c_devInstanceID=%%i"
+   echo i2c_devInstanceID="!i2c_devInstanceID!"
+   echo.
+ )
+ 
+ ::先强制删除inf文件再移除设备实例
+ pnputil /delete-driver "%oemfilename%" /force
+ pnputil /remove-device "%i2c_devInstanceID%"
+ 
+pnputil /scan-devices
+echo scan-devices扫描设备ok
+echo.
+
+
+::验证是否卸载成功，
+pnputil /enum-devices /class {745a17a0-74d3-11d0-b6fe-00a0c90f57da}  /ids /relations >LogFIle\hid_dev.txt
+ echo.
+ 
 find/i "MouseLikeTouchPad_I2C" LogFIle\hid_dev.txt && (
      echo 卸载驱动失败。
      echo Failed to unload the driver. 
@@ -78,6 +111,8 @@ find/i "MouseLikeTouchPad_I2C" LogFIle\hid_dev.txt && (
      exit
 )
 
+
+:unInstSucceded
 del/f /q LogFIle\hid_dev.txt
 echo 卸载驱动成功
 echo Unload driver succeeded.
@@ -85,4 +120,3 @@ echo.
 echo UNDRV_OK >LogFIle\Return_UninstDrv.txt
 echo UNDRV_OK >LogFIle\UninstDrvSucceeded.txt
 echo.
-
